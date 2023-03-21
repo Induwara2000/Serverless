f6376eda4ee64b0777aababcb6ccda30cebfc886
Induwara2000 <128261461+Induwara2000@users.noreply.github.com>
2023-03-21 23:05:00 +0530

    AWS Provider Code
    Code to install AWS provider version 3.70.0v

---
+++ aws.tf
@@ -0,0 +1,16 @@
+# #Installing AWS Provider version 3.70.0
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "3.70.0"
+#     }
+#   }
+# }
+
+# provider "aws" {
+#   # Configuration options
+# }
+
+
